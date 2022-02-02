6c60e30f3f06df51f644e11378912ffb1141f9a7
Victoria Rosca <curliucvictoria@gmail.com>
2022-02-02 18:19:19 +0200

    master

---
+++ src/lesson4Homework/Variables.java
@@ -0,0 +1,21 @@
+package lesson4Homework;
+
+public class Variables {
+    public static void main(String[] args) {
+        long currentWorldPopulation = 7811080815L;
+        long carsProducedThisYear = 55235358;
+        String firstLatterInAlphabet = "A";
+        boolean javaIsAmazing = true;
+        float pi = 3.14159f;
+
+
+        System.out.println(currentWorldPopulation);
+        System.out.println(carsProducedThisYear);
+        System.out.println(firstLatterInAlphabet);
+        System.out.println(javaIsAmazing);
+        System.out.println(pi);
+
+
+
+    }
+ }
