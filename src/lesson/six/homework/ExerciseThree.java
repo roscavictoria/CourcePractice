7c9dd84548c310e8f3d83cb918a1cdd3fc185b30
Victoria Rosca <curliucvictoria@gmail.com>
2022-02-09 12:20:58 +0200

    My first commit

---
+++ src/lesson/six/homework/ExerciseThree.java
@@ -0,0 +1,10 @@
+package lesson.six.homework;
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        System.out.println("Print numbers from 1 to 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
---
+++ src/lesson/six/homework/TernaryOperatorDemo5.java
@@ -0,0 +1,20 @@
+package lesson.six.homework;
+
+
+import static javafx.scene.paint.Color.color;
+
+public class TernaryOperatorDemo5 {
+    public static void main (String[] args) {
+        String colorCode;
+        color( "101");
+    }
+        public static void color(String colorCode) {
+            String color;
+
+            color = colorCode.equals ("100")? "Yellow" : colorCode.equals("101")? "Green" : colorCode.equals("102")?
+                    "Red" : "Invalid";
+            System.out.println("Color with number" + colorCode + " is :" +color);
+
+        }
+    }
+
